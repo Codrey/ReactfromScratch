b2b0642a43c8fe0efaa4e4162dc006a8bc36988c
Codrey <coreywinter@mac.com>
2018-09-15 23:05:59 +0100

    setup

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+               <h1>My React from Scratch App</h1>
+            </div>
+        );
+    }
+}
+
+
+
+
+export default App;
